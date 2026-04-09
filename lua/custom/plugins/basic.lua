25b948460eb5906180a375c9fa6e239d1058628b
Steffen Beisenherz <git@beisenherz.dev>
2026-04-09 16:20:46 +0200

    unpin nvim-surround

--- lua/custom/plugins/basic.lua
+++ lua/custom/plugins/basic.lua
@@ -2,13 +2,18 @@ return {
   { 'tpope/vim-sleuth' },
   {
     'kylechui/nvim-surround',
-    version = '^3.0.0', -- Use for stability; omit to use `main` branch for the latest features
     event = 'VeryLazy',
     opt = {},
   },
 
-  { 'JoosepAlviste/nvim-ts-context-commentstring', opts = { enable_autocmd = false } },
-  { 'NoahTheDuke/vim-just', ft = { 'just' } },
+  {
+    'JoosepAlviste/nvim-ts-context-commentstring',
+    opts = { enable_autocmd = false },
+  },
+  {
+    'NoahTheDuke/vim-just',
+    ft = { 'just' },
+  },
   {
     'lewis6991/gitsigns.nvim',
     opts = {
